b4a0e586a2847c447f4735a4c7e6c25865ce45d4
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-03-16 13:47:22 +0900

    webpack-clean 추가

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@
 
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry :
@@ -21,7 +22,7 @@ module.exports = {
     output : {
         // [name]은 entry에서 선언 된 object의 key를 파일명으로 하겠다는 것.
         filename : '[name].js',
-        path : path.resolve(__dirname,'dist'),
+        path : path.resolve(__dirname,'dist/js'),
         // publicPath 는 web상에서 배포될 때의 경로를 뜻 한다.
         // publicPath : '/'
     },
@@ -42,7 +43,9 @@ module.exports = {
             chunks : ['account'],
             inject : true,
             title : 'account template',
-            filename : path.join(__dirname,'dist/account.html')
-        })
+            filename : path.join(__dirname,'dist/html/account/account.html')
+        }),
+        // __dirname 은 현재 작업중인 path 를 return 해줌
+        new cleanWebpackPlugin(path.join(__dirname,'dist'))
     ]
 }
